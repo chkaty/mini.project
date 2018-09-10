30387dae8e0509759031ef76a79ea98c54235f98
liu-liuz <1214katychen@gmail.com>
2018-09-10 14:11:11 -0600

    moving rect added

---
+++ p5/sketch.js
@@ -0,0 +1,33 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let x;
+let dx;
+let rectWidth;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x = width/2;
+  dx = 5;
+  rectWidth= 200;
+}
+
+function draw() {
+  background(0,100,100);
+  //move rect
+  x += dx;
+
+  //check if hit the wall
+  if (x > width - rectWidth || x<0){
+    dx = dx * -1
+  }
+
+  //display ract
+  fill(0,255,0);
+  rect(x,400,rectWidth,150);
+
+}
