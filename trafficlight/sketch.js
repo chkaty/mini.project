0ea7235341700f9dcbec60d3206f90c4c466caad
liu-liuz <43077074+liu-liuz@users.noreply.github.com>
2018-09-25 21:36:51 -0600

    changed

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -6,48 +6,52 @@
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/
 
-let duration = 1000;
 let time;
-let state;
+let duration = 2;
+let state = 1;
 
 function setup() {
   createCanvas(600, 600);
 }
 
 function draw() {
-  let time = millis();
   background(255);
   drawOutlineOfLights();
   determineState();
+  colour();
+}
+
+
+function colour(){
   if (state === 1){
+    fill(225,0,0);
     ellipse(width/2, height/2 - 65, 50, 50);
   }
   else if (state === 2){
+    fill(0,225,0);
     ellipse(width/2, height/2, 50, 50);
   }
-  else{
+  else if (state === 3){
+    fill(255,204,0);
     ellipse(width/2, height/2 + 65, 50, 50);
   }
-  console.log(time);
-  console.log(state);
-
 }
+
 
 function determineState(){
-  let state = 0;
-  if(state === 0 && time%1000 === 0){
-    fill(225, 0, 0);
-    let state = 1;
+  let time = round(millis()/1000);
+  console.log(time);
+  if (time % 6 === 0){
+    state = 3;
   }
-  else if (state === 1 && time%2000 === 0){
-    fill(0, 225, 0);
-    let state = 2;
+  else if (time % 4 === 0){
+    state = 2;
   }
-  else if (state === 2 && time%3000 === 0){
-    fill(255, 204, 0);
-    let state = 0;
+  else if (time % 2 === 0){
+    state = 1;
   }
 }
+
 
 function drawOutlineOfLights() {
   //box
