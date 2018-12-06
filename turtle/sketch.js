a72696ae998f38a421e9a6aa72c7e7fd18a57d32
liu-liuz <43077074+liu-liuz@users.noreply.github.com>
2018-12-05 19:15:35 -0600

    turtle starting page

--- turtle/sketch.js
+++ turtle/sketch.js
@@ -1,9 +1,9 @@
-// Project Title
-// Your Name
-// Date
+// Turtle and Shape
+// Katy Chen && Marghoob Dar
+// 2018/12/5
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Use functions in list, input and collision detection
 
 let turtle = { pos: {
   x: -8,
@@ -24,6 +24,14 @@ let forwardDone;
 let turnDone;
 let lines = [];
 let input,button,movement;
+let speed = 60;
+let turtleStart = false;
+let start = true;
+let shapeStart = false;
+let askInput = false;
+let instruction = 0;
+let hitboxColour1;
+let hitboxColour2;
 
 
 function setup() {
@@ -31,57 +39,82 @@ function setup() {
   angleMode(DEGREES);
 
   functions = [];
-
-  input = createInput();
-  input.position(15, 50);
-
-  button = createButton("submit");
-  button.position(input.x + input.width, 50);
-  button.mousePressed(inputValue);
-
-  movement = createElement("h2", "Movement?");
-  movement.position(15, 0);
-
-  textAlign(CENTER);
-  textSize(25);
-  alert("You can input: forward(), turn(), hideTurtle(), showTurtle(), penDown(), penUp().");
 }
 
 
 function draw() {
-  translate(width/2,height/2);
-  background(220);
-  frameRate(90);
 
-  for(let j = 0; j < lines.length; j++){
-    translate(lines[j].trans.x,lines[j].trans.y);
-    rotate(lines[j].rota);
-    if(lines[j].penDown){
-      line(lines[j].x,lines[j].y,lines[j].x1,lines[j].y1);
-    }
+  if(start){
+    //creat a starting page to let user choose from turtle and shape
+    startPage();
   }
 
+  if(turtleStart){
+    backButton();
+    //ask user to input different kinds of function
+    if(askInput){
+      background(220);
+      input = createInput();
+      input.position(15, 50);
 
-  for(let i = functions.length-1; i>=0; i--){
-    if(functions[i] === forward &&!forwardDone){
-      forward(number);
+      button = createButton("submit");
+      button.position(input.x + input.width, 50);
+      button.mousePressed(inputValue);
+
+      movement = createElement("h2", "Movement?");
+      movement.position(15, 0);
+
+      textAlign(CENTER);
+      textSize(25);
+      askInput = false;
     }
-    else if(functions[i] === turn && !turnDone){
-      turn(number);
+
+    translate(width/2,height/2);
+    background(220);
+    //change speed
+    frameRate(speed);
+
+    //draw the path after function is done
+    for(let j = 0; j < lines.length; j++){
+      translate(lines[j].trans.x,lines[j].trans.y);
+      rotate(lines[j].rota);
+      if(lines[j].penDown){
+        line(lines[j].x,lines[j].y,lines[j].x1,lines[j].y1);
+      }
     }
-    if(forwardDone){
-      forwardDone = !forwardDone;
-      functions.splice(i,1);
+
+    //excute the function and delete it form the list when it's done
+    for(let i = functions.length-1; i>=0; i--){
+      if(functions[i] === forward && !forwardDone){
+        forward(number);
+      }
+      else if(functions[i] === turn && !turnDone){
+        turn(number);
+      }
+
+
+      if(forwardDone){
+        forwardDone = !forwardDone;
+        functions.splice(i,1);
+      }
+      if(turnDone){
+        turnDone = !turnDone;
+        functions.splice(i,1);
+      }
     }
-    if(turnDone){
-      turnDone = !turnDone;
-      functions.splice(i,1);
-    }
+    turtleVisible();
   }
-  turtleVisible();
+
+  if(shapeStart){
+    //not done yet
+    backButton();
+    background(220);
+    fill(0);
+    text("...",width/2,height/2);
+  }
 }
 
-
+//display turtle
 function turtleVisible(){
   if(turtle.visible){
     fill("green");
@@ -89,6 +122,7 @@ function turtleVisible(){
   }
 }
 
+//move forward
 function forward(distance) {
   if(turtle.newX + distance > turtle.pos.x){
     turtle.pos.x += 1;
@@ -105,14 +139,18 @@ function forward(distance) {
 }
 
 function turn(angle){
+  //set turtle position to zero(centered)
   turtle.pos.x = -8;
   turtle.pos.y = -10;
   turtle.newX = -8;
+
   if(turtle.angle > -angle){
     translate(turtle.distance,0);
     turtle.angle -= 1;
     rotate(turtle.angle);
   }
+
+  //when move to the angle asked, record the information to list
   else{
     let myLine = { trans: {
       x: turtle.distance,
@@ -125,10 +163,16 @@ function turn(angle){
     y1:0
     };
     lines.push(myLine);
+
+
     turnDone = true;
     forwardDone = false;
+
+    //move turtle
     translate(turtle.distance,0);
     rotate(turtle.angle);
+
+    //set angle and distance zero to prepare for next movement
     turtle.angle = 0;
     turtle.distance = 0;
   }
@@ -152,6 +196,7 @@ function lineVisible(value){
   line(0,0,turtle.distance,0);
 }
 
+//inputs user can enter
 function inputValue(){
   myFunction = input.value();
 
@@ -181,8 +226,13 @@ function inputValue(){
     turtle.penDown = false;
   }
 
+  if(myFunction.indexOf("speed") > -1){
+    checkNumber();
+    speed = 60*(number/10);
+  }
 }
 
+//find the number in input
 function checkNumber(){
   number = "";
   for(let i = 0; i < myFunction.length; i++){
@@ -193,6 +243,77 @@ function checkNumber(){
   number = int(number);
 }
 
+function startPage(){
+  let w = 100/400*width;
+  let h = 20/400*width;
+  let x = width/2-w/2;
+  let y =	height/5*2;
+
+  background(220);
+  textSize(15/400*width);
+  textAlign(CENTER);
+
+  let hitbox1 = collidePointRect(mouseX, mouseY, x, y, w, h);
+
+  if (hitbox1) {
+    fill(0);
+    hitboxColour1 = 255;
+    if(mouseIsPressed){
+      turtleStart = true;
+      askInput = true;
+      instruction += 1;
+      if(instruction === 3){
+        alert("You can input: forward(unlimited), turn(0-360), hideTurtle(), showTurtle(), penDown(), penUp(), speed(0-10).");
+        start = false;
+      }
+    }
+  }
+
+  else {
+    fill(255);
+    hitboxColour1 = 0;
+  }
+  rect(x, y, w, h);
+  fill(hitboxColour1);
+  text("Turtle",width/2,y+h/1.25);
+
+  y =	height/5*3;
+  let hitbox2 = collidePointRect(mouseX, mouseY, x, y, w, h);
+  if (hitbox2) {
+    fill(0);
+    hitboxColour2 = 255;
+    if(mouseIsPressed){
+      shapeStart = true;
+      start = false;
+    }
+  }
+  else {
+    fill(255);
+    hitboxColour2 = 0;
+  }
+  rect(x, y, w, h);
+  fill(hitboxColour2);
+  text("Shape",width/2,y+h/1.25);
+}
+
+//button that could return to start page
+function backButton(){
+  button = createButton("Back");
+  button.position(width/15*14, 20);
+  button.mousePressed(back);
+}
+
+function back(){
+  removeElements();
+  turtleStart = false;
+  start = true;
+  shapeStart = false;
+  askInput = false;
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+//Katy: made the choice turtle in this program and the possible way to combine two different choise
+//Marghoob: made the choice shape in this program
