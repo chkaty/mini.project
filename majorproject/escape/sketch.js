481aceb47073023d46ee9fa73d7201a4fb2e0b2f
liu-liuz <1214katychen@gmail.com>
2018-12-17 14:12:33 -0600

    buttom added

--- majorproject/escape/sketch.js
+++ majorproject/escape/sketch.js
@@ -9,46 +9,56 @@ let object;
 let time = 0;
 let tools = [];
 let frame;
+let table, tableZoom;
 let backgroundImage;
 function preload() {
-  object = loadAnimation("assets/object.png");
   backgroundImage = loadImage("assets/background.png");
 }
 
 function setup() {
   createCanvas(1366, 768);
   frame = loadImage("assets/frame.png");
+  door = loadImage("assets/door.png");
 
-  object = createSprite(width/2, height/2);
-  object.addAnimation("normal","assets/object.png");
-  object.addAnimation("turn","assets/triangle.png");
-  object.setCollider("rectangle", 0, 0, 200, 200);
-  object.onMousePressed = function() {
-    this.changeAnimation("turn");
-    time += 1;
+  table = createSprite(270,550);
+  table.addAnimation("normal","assets/table.png");
+  table.setCollider("rectangle", 0, 0, 100, 120);
+  table.onMousePressed = function() {
+    tableZoom = true;
   };
+
+  // object = createSprite(width/2, height/2);
+  // object.addAnimation("normal","assets/object.png");
+  // object.addAnimation("turn","assets/triangle.png");
+  // object.setCollider("rectangle", 0, 0, 200, 200);
+  // object.onMousePressed = function() {
+  //   this.changeAnimation("turn");
+  //   time += 1;
+  // };
 }
 
 function draw() {
   background(200);
   image(backgroundImage,50,50)
-  myCamera(object,200,200,400,400);
+  image(door,900,230)
+  myCamera(table);
   drawSprites();
-  if(time %2 !== 0){
-    object.addAnimation("turn","assets/triangle.png");
-  }
-  else{
-    object.addAnimation("turn","assets/object.png");
-  }
+  // if(time %2 !== 0){
+  //   object.addAnimation("turn","assets/triangle.png");
+  // }
+  // else{
+  //   object.addAnimation("turn","assets/object.png");
+  // }
   camera.off();
   image(frame, 0, 0);
 }
 
-function myCamera(sprite,x1,y1,x2,y2){
-  if(mouseIsPressed && mouseX > x1 && mouseY > y1 && mouseX < x2 && mouseY < y2){
-    camera.zoom = 2;
+function myCamera(sprite){
+  if(tableZoom === true){
+    camera.zoom = 3.5;
     camera.position.x = sprite.position.x;
     camera.position.y = sprite.position.y;
+    
   }
 }
 
